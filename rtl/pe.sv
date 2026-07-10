6d90851a3bc2720f038b1cb259409787b4d58024
Aditya-1020 <adityareddy400@gmail.com>
2026-07-10 17:09:17 +0530

    didnt handle sign properly

--- rtl/pe.sv
+++ rtl/pe.sv
@@ -2,61 +2,48 @@
 `timescale 1ps/1ps
 
 module pe #(
-    parameter int pe_data_w = 8, // activation/weight width
-    parameter int pe_acc_w = 17  // psum width
+    parameter DATA_WIDTH = 8,
+    parameter ACC_WIDTH = 32
 )(
     input wire clk,
     input wire rstn,
-    
-    input wire i_acc_clear, // clear accumulator
-
-    input wire i_w_load,
-    input wire signed [(pe_data_w-1):0] i_w,
-    output wire signed [(pe_data_w-1):0] o_w,
-
-    input wire signed [(pe_data_w-1):0] i_a,
-    output wire signed [(pe_data_w-1):0] o_a,
-
-    input wire signed [(pe_acc_w-1):0] i_psum,
-    output wire signed [(pe_acc_w-1):0] o_psum
+    input wire i_enable, // compute
+    input wire i_clear,// clear accumulator
+    input wire i_signed,
+    input wire [DATA_WIDTH-1:0] i_a,
+    output logic [DATA_WIDTH-1:0] o_a,
+    input wire [DATA_WIDTH-1:0] i_b,
+    output logic [DATA_WIDTH-1:0] o_b,
+    output wire [ACC_WIDTH-1:0] o_psum
 );
-    wire signed [(2*pe_data_w)-1:0] product;
-    wire signed [pe_acc_w-1:0] next_psum;
-
-    reg signed [pe_data_w-1:0] a_r;
-    reg signed [pe_data_w-1:0] w_r; // shift-register and stationary Store
-    reg signed [pe_acc_w-1:0] psum_r; 
-
-    assign product = i_a * w_r; // activation * stored weight
+    wire a_s_bit = i_signed ? i_a[DATA_WIDTH-1] : 1'd0;
+    wire b_s_bit = i_signed ? i_b[DATA_WIDTH-1] : 1'd0;
     
-    // accumulate
-    assign next_psum = i_psum + pe_acc_w'(product);
+    logic signed [ACC_WIDTH-1:0] accumulator;
+    wire signed [DATA_WIDTH:0] a_w = {a_s_bit, i_a};
+    wire signed [DATA_WIDTH:0] b_w = {b_s_bit, i_b};
+    
+    wire signed [2*DATA_WIDTH+1:0] mult_res = a_w * b_w;
 
     always_ff @(posedge clk or negedge rstn) begin
         if (!rstn) begin
-            a_r <= '0;
-            psum_r <= '0;
+            o_a <= '0;
+            o_b <= '0;
         end else begin
-            a_r <= i_a;
-            if (i_acc_clear && !i_w_load) begin
-                psum_r <= '0;
-            end else if (!i_w_load) begin
-                psum_r <= next_psum;
-            end
+            o_a <= i_a;
+            o_b <= i_b;
         end
     end
 
-    always_ff @(posedge clk) begin // pre load weight
-        if (i_w_load) begin
-            w_r <= i_w;
-        end else begin
-            w_r <= w_r; // hold weight
+    always @(posedge clk) begin
+        if (!rstn || i_clear) begin
+            accumulator <= '0;
+        end else if (i_enable) begin
+            accumulator <= accumulator + mult_res;
         end
     end
 
-    assign o_a = a_r;
-    assign o_psum = psum_r;
-    assign o_w = w_r; 
-    
+    assign o_psum = accumulator;
+
 endmodule
 `default_nettype wire
