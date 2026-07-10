48f3416515895fcfd5234340e78505ce1f455c02
Aditya-1020 <adityareddy400@gmail.com>
2026-07-10 23:28:46 +0530

    changes to PE and registered it as pre-pnr timing was too tight

--- constraints/pe.sdc
+++ constraints/pe.sdc
@@ -7,15 +7,16 @@ set clocks [get_clocks SYS_CLK]
 set resets [get_ports $reset_port]
 set clock_input [get_ports $clock_port]
 
-set_input_delay 0.5 -max -clock $clocks [get_ports {i_w i_a i_psum}]
-set_input_delay 0.5 -min -clock $clocks [get_ports {i_w i_a i_psum}]
+set_input_delay 1.0 -max -clock $clocks [get_ports {i_a i_b i_enable i_clear i_signed}]
+set_input_delay 1.0 -min -clock $clocks [get_ports {i_a i_b i_enable i_clear i_signed}]
 
 # ignore timing on reset port to registers (async reset)
 set_false_path -from $resets -to [all_registers]
 
-set_output_delay 1.0 -max -clock $clocks [get_ports {o_w o_a o_psum}]
+set_output_delay 1.0 -max -clock $clocks [get_ports {o_a o_b o_psum}]
 # set_output_delay 0.0 -min -clock $clocks [get_ports {o_w o_a o_psum}]
 
 report_checks -path_delay max -digits 3
+report_checks -path_delay min -digits 3
 report_wns
 report_tns
--- rtl/pe.sv
+++ rtl/pe.sv
@@ -11,39 +11,43 @@ module pe #(
     input wire i_clear,// clear accumulator
     input wire i_signed,
     input wire [DATA_WIDTH-1:0] i_a,
-    output logic [DATA_WIDTH-1:0] o_a,
     input wire [DATA_WIDTH-1:0] i_b,
-    output logic [DATA_WIDTH-1:0] o_b,
+    output wire [DATA_WIDTH-1:0] o_a,
+    output wire [DATA_WIDTH-1:0] o_b,
     output wire [ACC_WIDTH-1:0] o_psum
 );
-    wire a_s_bit = i_signed ? i_a[DATA_WIDTH-1] : 1'd0;
-    wire b_s_bit = i_signed ? i_b[DATA_WIDTH-1] : 1'd0;
-    
-    logic signed [ACC_WIDTH-1:0] accumulator;
-    wire signed [DATA_WIDTH:0] a_w = {a_s_bit, i_a};
-    wire signed [DATA_WIDTH:0] b_w = {b_s_bit, i_b};
-    
-    wire signed [2*DATA_WIDTH+1:0] mult_res = a_w * b_w;
-
-    always_ff @(posedge clk or negedge rstn) begin
-        if (!rstn) begin
-            o_a <= '0;
-            o_b <= '0;
-        end else begin
-            o_a <= i_a;
-            o_b <= i_b;
-        end
+  
+    logic [DATA_WIDTH-1:0] a_r, b_r;
+    always_ff @(posedge clk) begin
+        a_r <= i_a;
+        b_r <= i_b;
     end
 
+    wire a_s_bit = i_signed ? i_a[DATA_WIDTH-1] : 1'd0;
+    wire b_s_bit = i_signed ? i_b[DATA_WIDTH-1] : 1'd0;
+
+    wire signed [DATA_WIDTH:0] a_w = {a_s_bit, a_r};
+    wire signed [DATA_WIDTH:0] b_w = {b_s_bit, b_r};
+
+    logic signed [2*DATA_WIDTH+1:0] mult_r;
+    always_ff @(posedge clk) begin
+        if (i_enable) begin
+            mult_r <= a_w * b_w;
+        end
+    end
+    
+    logic signed [ACC_WIDTH-1:0] accumulator;
     always @(posedge clk) begin
         if (!rstn || i_clear) begin
             accumulator <= '0;
         end else if (i_enable) begin
-            accumulator <= accumulator + mult_res;
+            accumulator <= accumulator + mult_r;
         end
     end
 
     assign o_psum = accumulator;
+    assign o_a = a_r;
+    assign o_b = b_r;
 
 endmodule
 `default_nettype wire
