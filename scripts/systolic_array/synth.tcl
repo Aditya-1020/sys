81390e098fde85db13651c4b35c3726edb82cd8d
Aditya-1020 <adityareddy400@gmail.com>
2026-07-13 11:15:10 +0530

    systolci array synthesis script

---
+++ scripts/systolic_array/synth.tcl
@@ -0,0 +1,12 @@
+yosys -import
+set libfile $::env(TT_STA_LIB)
+read_verilog -lib tb/sram_stub.sv
+read_verilog verilog_sv2v/pe.v verilog_sv2v/systolic_array.v
+synth -top systolic_array
+dfflibmap -liberty $libfile
+abc -liberty $libfile
+opt -full -fast
+opt_clean
+opt_merge
+stat
+write_verilog -noattr build/systolic_array_synth.v
