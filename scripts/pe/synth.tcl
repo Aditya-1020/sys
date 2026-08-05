56f070173982732077f8d66da1f6525af21883a4
Aditya-1020 <adityareddy400@gmail.com>
2026-08-06 00:44:26 +0530

    mirror for icg

--- scripts/pe/synth.tcl
+++ scripts/pe/synth.tcl
@@ -4,6 +4,7 @@ set libfile $::env(TT_SYNTH_LIB)
 read_verilog verilog_sv2v/pe.v
 synth -top pe -flatten
 opt -full
+clockgate -min_net_size 2 -pos sky130_fd_sc_hd__dlclkp_2 GATE:CLK:GCLK
 dfflibmap -liberty $libfile
 abc -liberty $libfile -D 8000
 opt_clean -purge
