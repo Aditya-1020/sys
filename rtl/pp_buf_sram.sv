c8e01c721e6f9d33bef5c5cc8e4e95eec6775e2c
Aditya-1020 <adityareddy400@gmail.com>
2026-08-14 15:59:21 +0530

    added input registering to fix post flow violations

--- rtl/pp_buf_sram.sv
+++ rtl/pp_buf_sram.sv
@@ -18,14 +18,35 @@ module pp_buf_sram (
 );
 	wire [31:0] m0_dout, m1_dout;
 
-	wire m0_rstb, m0_access_en, m1_rstb, m1_access_en;
-	sram_rst_rel u_rel_m0 (.clk(clk), .rstn(rstn), .o_rstb(m0_rstb), .o_access_en(m0_access_en));
-	sram_rst_rel u_rel_m1 (.clk(clk), .rstn(rstn), .o_rstb(m1_rstb), .o_access_en(m1_access_en));
+	(* keep *)	wire m0_rstb, m0_access_en, m1_rstb, m1_access_en;
+	(* keep *)	sram_rst_rel u_rel_m0 (.clk(clk), .rstn(rstn), .o_rstb(m0_rstb), .o_access_en(m0_access_en));
+	(* keep *)	sram_rst_rel u_rel_m1 (.clk(clk), .rstn(rstn), .o_rstb(m1_rstb), .o_access_en(m1_access_en));
 
 	(* keep *) logic ping_pong_sel_r;
 	(* keep *) logic ping_pong_sel_m0_r;
 	(* keep *) logic ping_pong_sel_m1_r;
 	logic ping_pong_sel_d1_r;
+
+	logic [31:0] dma_wdata_r;
+	logic [3:0] dma_mask_r;
+	logic dma_cs_r, dma_we_r;
+	logic [5:0] dma_addr_r;
+
+	always_ff @(posedge clk or negedge rstn) begin
+	    if (!rstn) begin
+	        dma_cs_r <= 1'b0;
+	        dma_we_r <= 1'b0;
+	    end else begin
+	        dma_cs_r <= i_dma_cs;
+	        dma_we_r <= i_dma_we;
+	    end
+	end
+
+	always_ff @(posedge clk) begin
+	    dma_wdata_r <= i_dma_wdata;
+	    dma_mask_r  <= i_dma_mask;
+	    dma_addr_r  <= i_dma_addr;
+	end
 
 	always_ff @(posedge clk or negedge rstn) begin
 		if (!rstn) begin
@@ -48,22 +69,22 @@ module pp_buf_sram (
 	wire m0_dma = (ping_pong_sel_m0_r == 1'b0);
 	wire m1_dma = (ping_pong_sel_m1_r == 1'b1);
 
-	wire m0_ce = m0_access_en && (m0_dma ? i_dma_cs : i_cs_array);
-	wire m0_we = m0_dma ? i_dma_we : 1'b0;
-	wire [5:0] m0_addr = m0_dma ? i_dma_addr : i_addr_array;
+	wire m0_ce = m0_access_en && (m0_dma ? dma_cs_r : i_cs_array);
+	wire m0_we = m0_dma ? dma_we_r : 1'b0;
+	wire [5:0] m0_addr = m0_dma ? dma_addr_r : i_addr_array;
 
-	wire m1_ce = m1_access_en && (m1_dma ? i_dma_cs : i_cs_array);
-	wire m1_we = m1_dma ? i_dma_we : 1'b0;
-	wire [5:0] m1_addr = m1_dma ? i_dma_addr : i_addr_array;
+	wire m1_ce = m1_access_en && (m1_dma ? dma_cs_r : i_cs_array);
+	wire m1_we = m1_dma ? dma_we_r : 1'b0;
+	wire [5:0] m1_addr = m1_dma ? dma_addr_r : i_addr_array;
 
 	sram22_64x32m4w8 u_m0 (
 		.clk(clk),
 		.rstb(m0_rstb),
 		.ce(m0_ce),
 		.we(m0_we),
-		.wmask(i_dma_mask),
+		.wmask(dma_mask_r),
 		.addr(m0_addr),
-		.din(i_dma_wdata),
+		.din(dma_wdata_r),
 		.dout(m0_dout)
 	);
 
@@ -72,9 +93,9 @@ module pp_buf_sram (
 		.rstb(m1_rstb),
 		.ce(m1_ce),
 		.we(m1_we),
-		.wmask(i_dma_mask),
+		.wmask(dma_mask_r),
 		.addr(m1_addr),
-		.din(i_dma_wdata),
+		.din(dma_wdata_r),
 		.dout(m1_dout)
 	);
 
